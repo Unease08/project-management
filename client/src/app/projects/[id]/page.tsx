ba012e6ec237047d972861d7a5d3625cca356d50
Anish Shrestha <anisheyyy@gmail.com>
2025-06-17 16:40:51 +0545

    added project header in the project section

--- client/src/app/projects/[id]/page.tsx
+++ client/src/app/projects/[id]/page.tsx
@@ -1,7 +1,23 @@
-import React from "react";
+"use client";
 
-const Project = (props: Props) => {
-  return <div>Project</div>;
+import React, { useState } from "react";
+import ProjectHeader from "@/app/projects/ProjectHeader";
+
+type Props = {
+  params: { id: string };
+};
+
+const Project = ({ params }: Props) => {
+  const { id } = params;
+  const [activeTab, setActiveTab] = useState("Board");
+  const [isModalNewTaskOpen, setIsModalNewTaskOpen] = useState(false);
+  return (
+    <div>
+      {/* Modal New Tasks */}
+
+      <ProjectHeader activeTab={activeTab} setActiveTab={setActiveTab} />
+    </div>
+  );
 };
 
 export default Project;
